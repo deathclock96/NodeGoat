248575cd959180be7544c1e3acf0e5b1dd0b953f
Anthony McMellen <anthony@BHB-CSET-18-13.local>
2019-03-18 08:17:45 -0400

    made changes for A8-Cross-Site Request Forgery

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var favicon = require("serve-favicon");
 var bodyParser = require("body-parser");
 var session = require("express-session");
-// var csrf = require('csurf');
+var csrf = require("csurf");
 var consolidate = require("consolidate"); // Templating library adapter for Express
 var swig = require("swig");
 // var helmet = require("helmet");
@@ -97,7 +97,6 @@ MongoClient.connect(config.db, function(err, db) {
 
     }));
 
-    /*
     // Fix for A8 - CSRF
     // Enable Express csrf protection
     app.use(csrf());
@@ -106,7 +105,6 @@ MongoClient.connect(config.db, function(err, db) {
         res.locals.csrftoken = req.csrfToken();
         next();
     });
-    */
 
     // Register templating engine
     app.engine(".html", consolidate.swig);
